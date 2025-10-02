44eace7d0e42334647b80519b916b6f0fc0d8b32
Carlos Barria Young <cbarria@yahoo.com>
2025-10-01 23:06:57 -0300

    Fix spec_helper_acceptance to use voxpupuli-acceptance - Replace beaker-rspec/beaker-puppet with voxpupuli helper - Use configure_beaker block for setup - Properly install puppet with puppet_collection parameter

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -1,14 +1,9 @@
 # frozen_string_literal: true
 
-require 'beaker-rspec'
-require 'beaker-puppet'
-require 'beaker/puppet_install_helper'
-require 'beaker/module_install_helper'
+require 'voxpupuli/acceptance/spec_helper_acceptance'
 
-run_puppet_install_helper
-install_module_on(hosts)
-install_module_dependencies_on(hosts)
-
-RSpec.configure do |c|
-  c.formatter = :documentation
+configure_beaker do |host|
+  install_puppet_on(host, puppet_collection: 'puppet8') unless ENV['BEAKER_provision'] == 'no'
+  install_module_on(host)
+  install_module_dependencies_on(host)
 end
